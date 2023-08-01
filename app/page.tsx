b9debf4d9277ac70a6e0261fd5f922363ca4e22c
Uamirfarooq <umair.codedeveloper@gmail.com>
2023-08-01 09:35:07 -0700

    grafbase schema

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    
+    <section className='flex flex-col paddings mb-16'>
+        <h1>Categories</h1>
+        <h1>posts</h1>
+        <h1>LoadMOre</h1>
+    </section>
+    </>
+  )
+}
+
+export default Home
